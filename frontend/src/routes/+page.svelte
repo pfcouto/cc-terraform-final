5e68c766aeb23f4d7a1d0ba238bbdc1c8d8fb413
Pedro Couto <pedrocouto2001@hotmail.com>
2023-04-27 17:28:36 +0100

    mudanca

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 <div class="p-8">
 	<div class="grid h-screen place-items-center">
 		<h2 class="text-4xl font-extrabold dark:text-white">
-			Cloud Computing
+			Cloud Computing - Change
 		</h2>
 		<p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Engenharia Informática - Computação Móvel</p>
 	</div>
